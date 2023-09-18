a5805c5b228a79c00a93bc6d91ed8c611fecbe4d
詹曜宇 <zhan332066@gmail.com>
2023-09-18 23:17:07 +0800

    feat:consulation component first version

---
+++ app/(primary)/newjourney/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Consultation from "@/components/consultation/Consultation";
+const page = () => {
+  return (
+    <div className="relative h-full w-full">
+      <Consultation />
+    </div>
+  );
+};
+
+export default page;
