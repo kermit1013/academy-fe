98f3596b3d554858e91e6c2fcd49880b49c0ec5a
詹曜宇 <zhan332066@gmail.com>
2023-12-15 00:38:07 +0800

    feat: move email column to result page

--- app/(primary)/result/page.tsx
+++ app/(primary)/result/page.tsx
@@ -4,9 +4,7 @@ import { message } from "antd";
 import axios from "axios";
 import useClusterPosition from "@/hooks/useClusterPosition";
 import { QRCode } from "antd";
-interface Props {
-  QList: QList;
-}
+import { useRouter } from "next/navigation";
 
 interface QList {
   Q1: string;
@@ -19,10 +17,9 @@ interface QList {
   Q8: string;
 }
 
-const Share = (prop: Props) => {
-  const [messageApi, contextHolder] = message.useMessage();
-  const { imgUrl, MyRoomId } = useClusterPosition();
-  const [userMail, setUserMail] = useState("");
+const Share = () => {
+  const { imgUrl } = useClusterPosition();
+  const router = useRouter();
   const handleDownload = () => {
     const a = document.createElement("a");
 
@@ -30,37 +27,16 @@ const Share = (prop: Props) => {
     a.setAttribute("href", imgUrl);
     a.click();
   };
-  const handleShare = async () => {
-    messageApi.open({
-      type: "success",
-      content: "已送出!"
-    });
-
-    const res = await axios.post(
-      "http://139.162.82.246:8080/result",
-      {
-        room: MyRoomId,
-        username: localStorage.getItem("user_name"),
-        screenShot: imgUrl,
-        email: userMail,
-        feedback: JSON.stringify(prop)
-      },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }
-    );
-    console.log(prop, res);
-  };
+  setTimeout(() => {
+    router.push("/start");
+  }, 15000);
   return (
     <div className="flex  h-screen w-screen flex-col items-center justify-center bg-black/50">
-      {contextHolder}
       <div className="flex h-fit w-3/5 flex-col gap-10 rounded-lg bg-white bg-gradient-to-r from-sky-500/30 to-indigo-500/30 p-14">
         <p className="text-center text-4xl font-bold">祝你自主學習順利喔！</p>
         <div className="flex  items-center justify-center gap-10">
           <img className=" h-80 w-96 rounded-lg border-2 bg-white" src={imgUrl} alt="screenshot" />
-          <div className="flex flex-col gap-2">
+          <div className="flex h-full flex-col justify-between gap-2">
             <button
               className=" rounded-md border-2 bg-blue-100 p-2 text-xl font-bold  hover:bg-blue-200"
               onClick={handleDownload}
@@ -70,29 +46,9 @@ const Share = (prop: Props) => {
             <button className=" rounded-md border-2 bg-blue-100 p-2 text-xl font-bold hover:bg-blue-200">
               分享此網站給朋友
             </button>
-          </div>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-5">
-            <QRCode type="canvas" value="https://www.instagram.com/return_inn/" />
-            <p className=" text-xl font-bold">追蹤轉屋IG</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className=" text-xl font-bold">若想持續追蹤Loudy的新產品，請留下email</p>
-            <div className="flex gap-2">
-              <input
-                className="w-full rounded-md border pl-2"
-                type="text"
-                onChange={(event) => {
-                  setUserMail(event.target.value);
-                }}
-              />
-              <button
-                onClick={handleShare}
-                className="shrink-0 rounded-xl border border-green-400 bg-green-200 p-2 hover:border-2"
-              >
-                Enter
-              </button>
+            <div className="flex items-center gap-5">
+              <QRCode type="canvas" value="https://www.instagram.com/return_inn/" />
+              <p className=" text-xl font-bold">追蹤轉屋IG</p>
             </div>
           </div>
         </div>
@@ -104,6 +60,8 @@ const Share = (prop: Props) => {
 const Result = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const { imgUrl, MyRoomId } = useClusterPosition();
+  const [userMail, setUserMail] = useState("");
+
   const [isShare, setIsShare] = useState(false);
   const [Q1, SetQ1] = useState("");
   const [Q2, SetQ2] = useState("");
@@ -113,7 +71,6 @@ const Result = () => {
   const [Q6, SetQ6] = useState("");
   const [Q7, SetQ7] = useState("");
   const [Q8, SetQ8] = useState("");
-  const [toShare, SetToShare] = useState<Props>();
   const handleSubmit = async () => {
     const list: QList = {
       Q1,
@@ -129,7 +86,7 @@ const Result = () => {
       room: MyRoomId,
       username: localStorage.getItem("user_name"),
       screenShot: imgUrl,
-      email: "",
+      email: userMail,
       feedback: JSON.stringify(list)
     };
     console.log(data);
@@ -144,9 +101,8 @@ const Result = () => {
         content: "已送出!"
       });
     }
-    SetToShare({
-      QList: list
-    });
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("center");
     setIsShare(true);
   };
   return (
@@ -156,6 +112,16 @@ const Result = () => {
         <div className=" flex h-screen w-screen flex-col items-center justify-between gap-8 overflow-y-scroll p-12">
           <p className=" text-3xl font-bold">取得你的發想成果之前，給Loudy一些回饋吧!</p>
           <img className=" h-96 w-96 rounded-md border-2" src={imgUrl} alt="screenshot" />
+          <div className="flex flex-col gap-2">
+            <p className=" text-xl font-bold">若想持續追蹤Loudy的新產品，請留下email</p>
+            <input
+              className="w-full rounded-md border pl-2"
+              type="text"
+              onChange={(event) => {
+                setUserMail(event.target.value);
+              }}
+            />
+          </div>
           <div className=" flex h-fit w-2/3 flex-col gap-3">
             <div className="flex h-12 w-full justify-between">
               <p>
@@ -299,7 +265,7 @@ const Result = () => {
           </div>
         </div>
       ) : (
-        <Share QList={toShare!.QList} />
+        <Share />
       )}
     </div>
   );
