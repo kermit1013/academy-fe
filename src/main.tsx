8989735287d8c20c3389d30c38a73ea6215746c9
Kermit <k84101311@gmail.com>
2024-06-20 00:32:01 +0800

    rm duplicated tag

--- src/main.tsx
+++ src/main.tsx
@@ -18,9 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route
             index
             element={
-              <GoogleOAuthProvider clientId={clientId}>
                 <App />
-              </GoogleOAuthProvider>
             }
           />
           <Route path="forgetPwd" element={<PasswordRecoveryPage />} />
