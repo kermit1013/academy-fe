e7f8e6f8a43c0ce2b5540e52037be2d255335b78
Chia <lovemaple0913@gmail.com>
2024-10-06 02:13:15 +0800

    refactor: NavDrawer

--- src/components/NavDrawer.tsx
+++ src/components/NavDrawer.tsx
@@ -1,278 +1,260 @@
-import { useCallback, useEffect, useState } from 'react'
-import main_logo from '/nav_icons/logo.svg'
-import text_logo from '/groundi_text.svg'
-import setting from '/setting.svg'
-import icon_discord from '/discord.svg'
-import wavingHand from '/nav_icons/waving_hand.svg'
-import jamJar from '/nav_icons/jam_jar.svg'
-import idea from '/nav_icons/idea.svg'
-import rocket from '/nav_icons/rocket.svg'
-import dashboard from '/nav_icons/dashboard.svg'
-import file from '/nav_icons/file.svg'
-import delete_project from '/nav_icons/delete.svg'
 import { message } from 'antd'
-import useTopMenu from '../hooks/useTopMenu'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useStoreApi } from 'reactflow'
+
+import { getNodeClassName } from '../funcs/utils'
 import useAheadDiscord from '../hooks/useAheadDiscord'
+import useEdgesStateSynced from '../hooks/useEdgesStateSynced'
+import useEditor from '../hooks/useEditor'
+import useNodesStateSynced from '../hooks/useNodesStateSynced'
+import useStartProject from '../hooks/useStartProject'
+import useTopMenu from '../hooks/useTopMenu'
+import { DeleteProject, GetMyProject } from '../libs/api/project'
 import Editor from './Editor'
 import ProjectWall from './ProjectWall'
-import useStartProject from '../hooks/useStartProject'
-import useEditor from '../hooks/useEditor'
-import { useReactFlow, useStoreApi } from 'reactflow'
-import { DeleteProject, GetMyProject } from '../libs/api/project'
 
-import useNodesStateSynced from '../hooks/useNodesStateSynced'
-import { getNodeClassName } from '../funcs/utils'
-import useEdgesStateSynced from '../hooks/useEdgesStateSynced'
+import discord from '/discord.svg'
+import text_logo from '/groundi_text.svg'
+import dashboard from '/nav_icons/dashboard.svg'
+import delete_project from '/nav_icons/delete.svg'
+import file from '/nav_icons/file.svg'
+import idea from '/nav_icons/idea.svg'
+import jamJar from '/nav_icons/jam_jar.svg'
+import main_logo from '/nav_icons/logo.svg'
+import rocket from '/nav_icons/rocket.svg'
+import wavingHand from '/nav_icons/waving_hand.svg'
+import setting from '/setting.svg'
+
+const titleByGroupType = {
+  menuItems: '發想主題',
+  actionItems: '執行計畫'
+}
+
 interface Project {
   id: string | number
   name: string
   description: string
 }
 
-interface TallyPopupProps {
-  getPersonData: (type: number) => Promise<void>
-}
-
-
-const NavDrawer: React.FC<TallyPopupProps> = ({
-  getPersonData,
-}) => {
+const NavDrawer = () => {
   const [messageApi, contextHolder] = message.useMessage()
   const { setAheadDiscordStatus } = useAheadDiscord()
-  const { getNodes, getEdges } = useReactFlow()
   const setNodes = useNodesStateSynced()[1]
   const setEdges = useEdgesStateSynced()[1]
-  const [projects, setProjects] = useState([])
-  const { isOpen, setIsOpen, projectId, setProjectId, setEditable, nodeId } = useEditor()
+  const { selectedNode, setStartProjectStatus } = useStartProject()
+  const { isOpen, setIsOpen, projectId, setProjectId, setEditable, nodeId } =
+    useEditor()
   const store = useStoreApi()
   const {
+    isOpenTallyPopup,
     isOpenBrainStormContent,
     isOpenGalleryContent,
     isOpenProjectWall,
-    isOpenTallyPopup,
+    setIsOpenTallyPopup,
     setIsOpenBrainStormContent,
     setIsOpenGalleryContent,
     setIsOpenProjectWall,
-    setIsOpenSettingModal,
-    setIsOpenTallyPopup
+    setIsOpenSettingModal
   } = useTopMenu()
-  
-  const { selectedNode, setStartProjectStatus } = useStartProject()
 
+  const { getNodes, edges } = store.getState()
+  const nodes = getNodes()
+  // 判斷是否有第 2 層泡泡
+  const hasLevel2Bubble = useMemo(() => {
+    return nodes.some((node) => node.data.level === 2)
+  }, [nodes])
+  // 判斷是否有第 3 層泡泡
+  const hasLevel3Bubble = useMemo(() => {
+    return nodes.some((node) => node.data.level === 3)
+  }, [nodes])
+
+  const [projects, setProjects] = useState([])
   const [isExpanded, setIsExpanded] = useState(false)
-  const [prevContents, setPrevContents] = useState('')
 
-  const [hasLevel3Bubble, setHasLevel3Bubble] = useState(
-    getNodes().filter((node) => node.data.level == 3).length > 0 ? true : false
-  )
-
-  const handleProjectDelete = async (projectId: string | number) => {
-    if (confirm('確定刪除該專案嗎？') === false) return
-
-    DeleteProject(projectId)
-      .then(() => {
-        setProjects(
-          projects.filter((project: Project) => project.id !== projectId)
-        )
-        messageApi.success('專案刪除成功！')
-        window.location.reload()
-      })
-      .catch((error: any) => {
-        console.error('Error deleting project:', error)
-        messageApi.error('專案刪除失敗，請稍後再試。')
-      })
-  }
-
-  const initializeOpenContents = () => {
-    setIsOpenBrainStormContent(false);
-    setIsOpenGalleryContent(false);
-    setIsOpenProjectWall(false);
-    setIsOpenSettingModal(false);
-    setIsOpenTallyPopup(false);
-    if (prevContents==='gallery' && localStorage.getItem('gallery_user_id')) {
-      getPersonData(0)
-      setPrevContents('')
-    }
-  }
-
-  const handlerTallyPopup = () => {
-    initializeOpenContents();
-    setIsOpenTallyPopup(true);
-  }
-  const handlerChange2BrainStorm = useCallback(() => {
-    initializeOpenContents();
-    setIsOpenBrainStormContent(true);
-
-    const { getNodes } = store.getState()
-    if (getNodes().filter((node) => node.data.level === 2).length === 0) {
-      return messageApi.warning('請先新增第三層的心智圖泡泡！')
-    }
-
-    const RenderNewBubbleList = getNodes().map((node) => {
-      const newNode = { ...node }
-      newNode.className = getNodeClassName({
-        level: node.data.level,
-        is_launched: node.data.is_launched,
-        is_visible: node.data.is_visible,
-        isOpenBrainStormContent: true
-      })
-      return newNode
-    })
-    setNodes(RenderNewBubbleList)
-
-    const RenderNewEdgeList = getEdges().map((edge) => {
-      const newEdge = { ...edge }
-      newEdge.style = { stroke: '#c4c4c4', strokeWidth: 2 }
-      return newEdge
-    })
-    setEdges(RenderNewEdgeList)
+  // #region 處理訊息開啟功能
+  // 針對不同狀態的提示訊息
+  const checkOpenStatus = useCallback(() => {
+    if (isOpenTallyPopup) return '請先離開探索問卷！'
+    if (isOpenBrainStormContent) return '請先離開靈感果醬！'
+    if (isOpenGalleryContent) return '請先離開點子畫廊！'
+    if (isOpenProjectWall) return '請先離開專案畫廊！'
+    return null
   }, [
-    isOpenGalleryContent,
+    isOpenTallyPopup,
     isOpenBrainStormContent,
-    isOpenProjectWall,
-    isOpenTallyPopup
+    isOpenGalleryContent,
+    isOpenProjectWall
   ])
 
-
-  const handlerChange2Gallery = () => {
-    initializeOpenContents();
-    setIsOpenGalleryContent(true)
-    setPrevContents('gallery')
+  // 在執行函式前，檢查是否有開啟其它功能
+  const handleAction = (action: () => void) => {
+    const statusMessage = checkOpenStatus()
+    statusMessage ? messageApi.warning(statusMessage) : action()
   }
 
-  const handlerOpenProjectWall = () => {
-    initializeOpenContents();
-    setIsOpenProjectWall(true)
-  }
+  // 開啟 Setting Modal
+  const handlerSetting = () => setIsOpenSettingModal(true)
 
-  const handlerSetting = () => {
-    setIsOpenSettingModal(true)
-  }
+  // 開啟 Discord
+  const handlerAheadDiscordStatus = () => setAheadDiscordStatus(true)
 
-  const handlerAheadDiscordStatus = () => {
-    setAheadDiscordStatus(true)
-  }
+  // 開啟 Tally Popup (探索問卷)
+  const handlerTallyPopup = () => handleAction(() => setIsOpenTallyPopup(true))
 
-  const handlerTallyStartProject = () => {
-    if (!selectedNode) return messageApi.warning('請先選擇泡泡哦！')
-    if (!hasLevel3Bubble) {
-      return messageApi.warning(
-        '請先透過「探索問卷」產生心智圖，或自行新增心智圖泡泡！'
-      )
-    }
-    if (!selectedNode || selectedNode.data.label === '') {
-      return messageApi.warning('請填寫內容後在開始計劃！')
-    }
-    if (!selectedNode || selectedNode.data.level !== 3)
-      return messageApi.warning('請先選擇一個方形泡泡哦！')
-    if (!selectedNode || selectedNode.data.is_launched === true) {
-      return messageApi.warning('該方形泡泡已經與專案綁定！')
-    }
-    setStartProjectStatus(true)
-  }
+  // 開啟 BrainStorm Content (探索問卷)
+  const handlerChange2BrainStorm = () =>
+    handleAction(() => {
+      if (!hasLevel2Bubble) {
+        return messageApi.warning('請先新增第三層的心智圖泡泡！')
+      }
 
-  const handleProjectClick = 
-    (projectId: string) => {
-      initializeOpenContents()
+      const updateNodes = nodes.map((node) => ({
+        ...node,
+        className: getNodeClassName({
+          level: node.data.level,
+          is_launched: node.data.is_launched,
+          is_visible: node.data.is_visible,
+          isOpenBrainStormContent: true
+        })
+      }))
+
+      const updateEdges = edges.map((edge) => ({
+        ...edge,
+        style: { stroke: '#c4c4c4', strokeWidth: 2 }
+      }))
+
+      setNodes(updateNodes)
+      setEdges(updateEdges)
+      setIsOpenBrainStormContent(true)
+    })
+
+  // 開啟 Gallery Content (點子畫廊)
+  const handlerChange2Gallery = () =>
+    handleAction(() => setIsOpenGalleryContent(true))
+
+  // 開啟 Project Wall (專案畫廊)
+  const handlerOpenProjectWall = () =>
+    handleAction(() => setIsOpenProjectWall(true))
+
+  // 開啟 Project Editor
+  const handleProjectClick = (projectId: string) => {
+    handleAction(() => {
       setProjectId(projectId)
       setIsOpen(true)
       setEditable(true)
+    })
+  }
+
+  const handlerTallyStartProject = useCallback(() => {
+    const validations = [
+      {
+        isValid: () => selectedNode,
+        errorMessage: '請先選擇泡泡哦！'
+      },
+      {
+        isValid: () => hasLevel3Bubble,
+        errorMessage: '請先透過「探索問卷」產生心智圖，或自行新增心智圖泡泡！'
+      },
+      {
+        isValid: () => selectedNode?.data.label !== '',
+        errorMessage: '請填寫內容後在開始計劃！'
+      },
+      {
+        isValid: () => selectedNode?.data.level === 3,
+        errorMessage: '請先選擇一個方形泡泡哦！'
+      },
+      {
+        isValid: () => !selectedNode?.data.is_launched,
+        errorMessage: '該方形泡泡已經與專案綁定！'
+      }
+    ]
+
+    const invalidMessage = validations.find(
+      (validation) => !validation.isValid()
+    )?.errorMessage
+
+    invalidMessage
+      ? messageApi.warning(invalidMessage)
+      : setStartProjectStatus(true)
+  }, [selectedNode, hasLevel3Bubble])
+
+  // #endregion
+
+  const handleProjectDelete = async (projectId: string | number) => {
+    if (!confirm('確定刪除該專案嗎？')) return
+
+    try {
+      await DeleteProject(projectId)
+      setProjects(
+        projects.filter((project: Project) => project.id !== projectId)
+      )
+      messageApi.success('專案刪除成功！')
+      window.location.reload()
+    } catch (error) {
+      console.error('Error deleting project:', error)
+      messageApi.error('專案刪除失敗，請稍後再試。')
     }
+  }
 
-  useEffect(() => {
-    getProjects()
-  }, [])
-
-  useEffect(() => {
-    if (!isOpen) {
-      getProjects()
+  const getProjects = async () => {
+    try {
+      const result = await GetMyProject()
+      setProjects(result)
+    } catch (error) {
+      console.error('Error fetching data:', error)
     }
-  }, [isOpen])
+  }
 
-  const getProjects = useCallback(async () => {
-    const access_token = localStorage.getItem('access_token')
-    if (!access_token) {
-      return
-    }
-    GetMyProject()
-      .then((result) => {
-        setProjects(result)
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error)
-      })
-  }, [])
-
-
-  const menuItemsList = [
-    {
-      icon: <img src={wavingHand} alt="" />,
-      label: '探索問卷',
-      prompt: '透過問卷生成心智圖',
-      onClick: handlerTallyPopup
-    },
-    {
-      icon: <img src={jamJar} alt="" />,
-      label: '靈感果醬',
-      prompt: '透過即興遊戲生成專案主題',
-      onClick: handlerChange2BrainStorm,
-    },
-    {
-      icon: <img src={idea} alt="" />,
-      label: '點子畫廊',
-      prompt: '逛逛他人的心智圖',
-      onClick: handlerChange2Gallery
-    }
-  ]
-  const actionItemList =[
-    {
-      icon: <img src={rocket} alt="" />,
-      label: '開始計畫',
-      prompt: '請選擇一個專案主題開始',
-      onClick: handlerTallyStartProject,
-      disabled:
-        !selectedNode ||
-        selectedNode.data.is_launched ||
-        selectedNode.data.level !== 3
-    },
-    {
-      icon: <img src={dashboard} alt="" />,
-      label: '專案畫廊',
-      prompt: '逛逛他人的專案',
-      onClick: handlerOpenProjectWall
-    },
-    {
-      icon: <img src={icon_discord} alt="" />,
-      label: '社群互動',
-      prompt: '在 Discord 中提問、交流',
-      onClick: handlerAheadDiscordStatus
-    }
-  ]
-
-  const [menuItems, setMenuItems] = useState(menuItemsList)
-  const [actionItems, setActionItems] = useState(actionItemList)
-
-  useEffect(() => {
-    const { getNodes } = store.getState()
-    // const level_2_result =
-    //   getNodes().filter((node) => node.data.level === 2).length > 0
-    //     ? true
-    //     : false
-    const level_3_result =
-      getNodes().filter((node) => node.data.level === 3).length > 0
-        ? true
-        : false
-    // setHasLevel2Bubble(level_2_result)
-    setHasLevel3Bubble(level_3_result)
-    setMenuItems(menuItemsList)
-    setActionItems(actionItemList)
-  }, [
-    store,
-    isOpenGalleryContent,
-    isOpenBrainStormContent,
-    selectedNode,
-    hasLevel3Bubble
-  ])
+  const renderItemType = {
+    menuItems: [
+      {
+        icon: <img src={wavingHand} alt="" />,
+        label: '探索問卷',
+        prompt: '透過問卷生成心智圖',
+        onClick: !isOpenTallyPopup ? handlerTallyPopup : () => {},
+        disabled: isOpenTallyPopup
+      },
+      {
+        icon: <img src={jamJar} alt="" />,
+        label: '靈感果醬',
+        prompt: '透過即興遊戲生成專案主題',
+        onClick: !isOpenBrainStormContent ? handlerChange2BrainStorm : () => {},
+        disabled: isOpenBrainStormContent
+      },
+      {
+        icon: <img src={idea} alt="" />,
+        label: '點子畫廊',
+        prompt: '逛逛他人的心智圖',
+        onClick: !isOpenGalleryContent ? handlerChange2Gallery : () => {},
+        disabled: isOpenGalleryContent
+      }
+    ],
+    actionItems: [
+      {
+        icon: <img src={rocket} alt="" />,
+        label: '開始計畫',
+        prompt: '請選擇一個專案主題開始',
+        onClick: handlerTallyStartProject,
+        disabled:
+          !selectedNode ||
+          selectedNode.data.is_launched ||
+          selectedNode.data.level !== 3
+      },
+      {
+        icon: <img src={dashboard} alt="" />,
+        label: '專案畫廊',
+        prompt: '逛逛他人的專案',
+        onClick: !isOpenProjectWall ? handlerOpenProjectWall : () => {},
+        disabled: isOpenProjectWall
+      },
+      {
+        icon: <img src={discord} alt="" />,
+        label: '社群互動',
+        prompt: '在 Discord 中提問、交流',
+        onClick: handlerAheadDiscordStatus
+      }
+    ]
+  }
 
   const projectItems = projects.map((project: Project) => ({
     icon: <img src={file} alt="" />,
@@ -281,6 +263,16 @@ const NavDrawer: React.FC<TallyPopupProps> = ({
     onClick: () => handleProjectClick(project.id.toString()),
     onDelete: () => handleProjectDelete(project.id)
   }))
+
+  useEffect(() => {
+    if (!isOpen) {
+      getProjects()
+    }
+  }, [isOpen])
+
+  useEffect(() => {
+    getProjects()
+  }, [])
 
   return (
     <>
@@ -316,69 +308,51 @@ const NavDrawer: React.FC<TallyPopupProps> = ({
               className={`h-3 pl-2 ${isExpanded ? 'block' : 'hidden'}`}
             />
           </div>
-          <div
-            className={`mb-4 font-sans text-xs font-medium text-[#6CA579] ${isExpanded ? 'block' : 'text-center'}`}
-          >
-            發想主題
-          </div>
-          {menuItems.map((item, index) => (
-            <div
-              key={index}
-              className={`group mb-4 ml-3 flex cursor-pointer items-center justify-between ${
-                !isExpanded ? 'justify-center' : 'w-full pr-2'
-              }`}
-            >
-              <div
-                className={`tooltip tooltip-right flex font-sans`}
-                data-tip={item.prompt}
-                onClick={item.onClick}
-                onTouchStart={item.onClick}
-              >
-                <span className="text-2xl text-gray-400 group-hover:text-[#6CA579]">
-                  {item.icon}
-                </span>
-                <span
-                  className={`ml-3 flex items-center font-sans text-sm 
-                  } ${isExpanded ? 'block' : 'hidden'}`}
+
+          {Object.entries(renderItemType).map(([key, items], index) => (
+            <>
+              <section>
+                <div
+                  className={`mb-4 font-sans text-xs font-medium text-[#6CA579] ${isExpanded ? 'block' : 'text-center'}`}
+                  key={`${key}-${index}`}
                 >
-                  {item.label}
-                </span>
-              </div>
-            </div>
+                  {titleByGroupType[key as keyof typeof titleByGroupType]}
+                </div>
+                {items.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`group mb-4 ml-3 flex cursor-pointer items-center justify-between ${
+                      !isExpanded ? 'justify-center' : 'w-full pr-2'
+                    }`}
+                  >
+                    <div
+                      className={`tooltip tooltip-right flex font-sans ${item.disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                      data-tip={item.prompt}
+                      onClick={item.onClick}
+                      onTouchStart={item.onClick}
+                    >
+                      <span className="text-2xl text-gray-400 group-hover:text-[#6CA579]">
+                        {item.icon}
+                      </span>
+                      <span
+                        className={`ml-3 flex items-center font-sans text-sm ${
+                          item.disabled
+                            ? 'text-gray-300'
+                            : 'text-gray-600 group-hover:text-[#6CA579]'
+                        } ${isExpanded ? 'block' : 'hidden'}`}
+                      >
+                        {item.label}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </section>
+              {index < Object.keys(renderItemType).length - 1 && (
+                <div className="my-4 border-t border-gray-200" />
+              )}
+            </>
           ))}
-          <div className="my-4 border-t border-gray-200"></div>
-          <div
-            className={`mb-4 font-sans text-xs font-medium text-[#6CA579] ${isExpanded ? 'block' : 'text-center'}`}
-          >
-            執行計畫
-          </div>
-          {actionItems.map((item, index) => (
-            <div
-              key={index}
-              className={`group mb-4 ml-3 flex cursor-pointer items-center justify-between ${
-                !isExpanded ? 'justify-center' : 'w-full pr-2'
-              }`}
-            >
-              <div
-                className={`tooltip tooltip-right flex font-sans ${item.disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
-                data-tip={item.prompt}
-                onClick={item.onClick}
-              >
-                <span className="text-2xl text-gray-400 group-hover:text-[#6CA579]">
-                  {item.icon}
-                </span>
-                <span
-                  className={`ml-3 flex items-center font-sans text-sm ${
-                    item.disabled
-                      ? 'text-gray-300'
-                      : 'text-gray-600 group-hover:text-[#6CA579]'
-                  } ${isExpanded ? 'block' : 'hidden'}`}
-                >
-                  {item.label}
-                </span>
-              </div>
-            </div>
-          ))}
+
           <div className="my-4 border-t border-gray-200"></div>
           <div
             className={`mb-4 font-sans text-xs font-medium text-[#6CA579] ${isExpanded ? 'block' : 'text-center'}`}
