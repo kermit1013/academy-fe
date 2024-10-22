f1c075e5eb188e44fe0871682fa2e4d65299f90a
Kermit <k84101311@gmail.com>
2024-10-22 17:20:17 +0800

    oauth login refactored

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -1,8 +1,8 @@
-import { useGoogleLogin } from '@react-oauth/google' 
+import { useGoogleLogin } from '@react-oauth/google'
 import { useNavigate } from 'react-router-dom'
 import { message } from 'antd'
 import { PropsWithChildren } from 'react'
-import axios from 'axios'
+import { OauthGoogle } from '../libs/api/login'
 
 const GoogleLogin = ({ children }: PropsWithChildren) => {
   const [messageApi, contextHolder] = message.useMessage()
@@ -10,33 +10,26 @@ const GoogleLogin = ({ children }: PropsWithChildren) => {
 
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
-      try {
-        const result = await axios.post(
-          'https://api.loudy.in/api/users/auth-receiver',
-          {
-            credential: tokenResponse.access_token
-          }
-        )
-
-        if (result.status === 200) {
-          localStorage.setItem('access_token', result.data.data.access)
-          localStorage.setItem('refresh_token', result.data.data.refresh)
+      OauthGoogle(tokenResponse.access_token)
+        .then((result) => {
+          localStorage.setItem('access_token', result.data.access)
+          localStorage.setItem('refresh_token', result.data.refresh)
           messageApi.success('Google Sign-In successful!')
           setTimeout(() => {
-            navigate('/search')
+            navigate('/dashboard')
           }, 1000)
-        }
-      } catch (error) {
-        console.error('Error during Google Sign-In:', error)
-        messageApi.warning('Google Sign-In failed. Please try again.')
-      }
+        })
+        .catch((e) => {
+          console.error('Error during Google Sign-In:', e)
+          messageApi.warning('Google Sign-In failed. Please try again.')
+        })
     }
   })
   return (
     <>
       {contextHolder}
       <button
-        className="mt-4 w-full rounded-[20px] border-2 border-[#7B7C7B] bg-white/30 p-3 text-md text-[#7B7C7B] backdrop-blur-sm shadow-md hover:shadow-inner e-in-out transition duration-500 hover:-translate-x hover:scale-105"
+        className="text-md e-in-out hover:-translate-x mt-4 w-full rounded-[20px] border-2 border-[#7B7C7B] bg-white/30 p-3 text-[#7B7C7B] shadow-md backdrop-blur-sm transition duration-500 hover:scale-105 hover:shadow-inner"
         onClick={() => googleLogin()}
       >
         {children}
--- src/libs/api/login.ts
+++ src/libs/api/login.ts
@@ -10,7 +10,22 @@ export const TokenPair = async (
       password: passwd
     })
     if (result.status == 200) {
-      return result.data
+      return result
+    }
+  } catch (error: any) {
+    return error.response.data.detail
+  }
+}
+
+export const OauthGoogle = async (
+  credential: string,
+): Promise<any> => {
+  try {
+    const result = await request.post('/oauth/google', {
+      credential: credential,
+    })
+    if (result.status == 200) {
+      return result
     }
   } catch (error: any) {
     return error.response.data.detail
@@ -22,15 +37,15 @@ export const RegisterUser = async (
   email: string,
   passwd: string
 ): Promise<any> => {
-    const result = await request.post('/users', {
-      username: username,
-      gender: '',
-      email: email,
-      school: '',
-      grade: '',
-      password: passwd
-    })
-    if (result.status == 200) {
-      return result.data
-    }
+  const result = await request.post('/users', {
+    username: username,
+    gender: '',
+    email: email,
+    school: '',
+    grade: '',
+    password: passwd
+  })
+  if (result.status == 200) {
+    return result.data
+  }
 }
